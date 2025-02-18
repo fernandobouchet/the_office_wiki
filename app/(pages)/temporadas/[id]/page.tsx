07e4ae4d2a85d33b4a0b2eb4c851b0d47c5bffa9
fernandobouchet <fernandobouchet@gmail.com>
2025-02-17 23:32:58 -0300

    feat: update dynamic page after nextjs updgrade

--- app/(pages)/temporadas/[id]/page.tsx
+++ app/(pages)/temporadas/[id]/page.tsx
@@ -3,17 +3,16 @@ import getSeasonsById from "@/lib/getSeasonsById";
 import { Season } from "@/types";
 
 interface Props {
-  params: {
-    id: number;
-  };
+  params: Promise<{ id: string }>;
 }
 
 const Page = async ({ params }: Props) => {
-  const data: Season = await getSeasonsById(params.id.toString());
+  const { id } = await params;
+  const data: Season = await getSeasonsById(id.toString());
   return (
     <section className="main-section xl:max-w-(--breakpoint-xl)">
       <div className="pt-4 pb-8 text-center">
-        <h1 className="text-3xl font-bold mb-6">Temporada {params.id}</h1>
+        <h1 className="text-3xl font-bold mb-6">Temporada {id}</h1>
         <p>{data.overview}</p>
       </div>
       <div className="flex flex-col items-stretch sm:grid sm:grid-cols-2 lg:grid-cols-3 gap-4 ">
